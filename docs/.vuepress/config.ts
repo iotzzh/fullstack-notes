a86f60c0f8ff6a35b396ee3247a9d6341821e8ee
zhangzhonghua <956159241@qq.com>
2023-09-21 14:56:53 +0800

    添加评论插件

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -102,58 +102,58 @@ export default defineUserConfig({
     // 留言
     commentPlugin({
       provider: "Giscus",
-      repo :"itwanger/tobebetterjavaer-giscus",
-      repoId:"R_kgDOHBJssg",
-      category:"Announcements",
-      categoryId:"DIC_kwDOHBJsss4COJOx",
+      repo :"iotzzh/fullstack-notes-giscus",
+      repoId:"R_kgDOKWV5mQ",
+      category:"General",
+      categoryId:"DIC_kwDOKWV5mc4CZgi6",
     }),
     // 只能搜索
-    docsearchPlugin({
-      appId: "O566AMFNJH",
-      apiKey: "d9aebea8bd1a4f1e01201464bbab255f",
-      indexName: "tobebetterjavaer",
-      locales: {
-        "/": {
-          placeholder: "搜索文档",
-          translations: {
-            button: {
-              buttonText: "搜索文档",
-              buttonAriaLabel: "搜索文档",
-            },
-            modal: {
-              searchBox: {
-                resetButtonTitle: "清除查询条件",
-                resetButtonAriaLabel: "清除查询条件",
-                cancelButtonText: "取消",
-                cancelButtonAriaLabel: "取消",
-              },
-              startScreen: {
-                recentSearchesTitle: "搜索历史",
-                noRecentSearchesText: "没有搜索历史",
-                saveRecentSearchButtonTitle: "保存至搜索历史",
-                removeRecentSearchButtonTitle: "从搜索历史中移除",
-                favoriteSearchesTitle: "收藏",
-                removeFavoriteSearchButtonTitle: "从收藏中移除",
-              },
-              errorScreen: {
-                titleText: "无法获取结果",
-                helpText: "你可能需要检查你的网络连接",
-              },
-              footer: {
-                selectText: "选择",
-                navigateText: "切换",
-                closeText: "关闭",
-                searchByText: "搜索提供者",
-              },
-              noResultsScreen: {
-                noResultsText: "无法找到相关结果",
-                suggestedQueryText: "你可以尝试查询",
-              },
-            },
-          },
-        },
-      },
-    }),
+    // docsearchPlugin({
+    //   appId: "O566AMFNJH",
+    //   apiKey: "d9aebea8bd1a4f1e01201464bbab255f",
+    //   indexName: "tobebetterjavaer",
+    //   locales: {
+    //     "/": {
+    //       placeholder: "搜索文档",
+    //       translations: {
+    //         button: {
+    //           buttonText: "搜索文档",
+    //           buttonAriaLabel: "搜索文档",
+    //         },
+    //         modal: {
+    //           searchBox: {
+    //             resetButtonTitle: "清除查询条件",
+    //             resetButtonAriaLabel: "清除查询条件",
+    //             cancelButtonText: "取消",
+    //             cancelButtonAriaLabel: "取消",
+    //           },
+    //           startScreen: {
+    //             recentSearchesTitle: "搜索历史",
+    //             noRecentSearchesText: "没有搜索历史",
+    //             saveRecentSearchButtonTitle: "保存至搜索历史",
+    //             removeRecentSearchButtonTitle: "从搜索历史中移除",
+    //             favoriteSearchesTitle: "收藏",
+    //             removeFavoriteSearchButtonTitle: "从收藏中移除",
+    //           },
+    //           errorScreen: {
+    //             titleText: "无法获取结果",
+    //             helpText: "你可能需要检查你的网络连接",
+    //           },
+    //           footer: {
+    //             selectText: "选择",
+    //             navigateText: "切换",
+    //             closeText: "关闭",
+    //             searchByText: "搜索提供者",
+    //           },
+    //           noResultsScreen: {
+    //             noResultsText: "无法找到相关结果",
+    //             suggestedQueryText: "你可以尝试查询",
+    //           },
+    //         },
+    //       },
+    //     },
+    //   },
+    // }),
   ],
 
   theme,
